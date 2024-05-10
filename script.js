37f6841f5cbc75e88ddddb81477ecc9fefffe11a
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-10 16:37:40 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function changetext(){
+	document.getElementById('status').textContent = "Entered Metaverse";
+}
+document.getElementById('htmlbody').innerHTML += "<p id='status'>Enter the Metaverse</p>";
+document.getElementById('htmlbody').innerHTML += "<button id='enterBtn'>Enter</button>";
+document.getElementById('enterBtn').addEventListener("click",changetext);
