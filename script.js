86e00a272c4a5180085980465f9b0df45dd3d896
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-12 13:58:55 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 document.getElementById("enterBtn").addEventListener("click", function() {
-  let e = document.getElementById("status");
-  let d = document.createElement('h1');
-	d.innerHTML = "Entered Metaverse"
-	e.parentNode.insertBefore(d, e);
-e.parentNode.removeChild(e);
+  let statusElement = document.getElementById("status");
+  // change the tag and text of statusElement here
+	statusElement.innerHTML = "<h1>Entered Metaverse</h1>";
 });
