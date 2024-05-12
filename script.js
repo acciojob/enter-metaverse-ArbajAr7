1862ff88b05bab59c114721171c8d9901b214647
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-12 13:56:52 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-//your JS code here. If required.
-document.getElementById('enterBtn').addEventListener('click', function() {
-  var statusElement = document.getElementById('status');
-  var newElement = document.createElement('h1');
-  newElement.innerHTML = "Entered Metaverse";
-  statusElement.parentNode.replaceChild(newElement, statusElement);
+document.getElementById("enterBtn").addEventListener("click", function() {
+  let e = document.getElementById("status");
+  let d = document.createElement('h1');
+	d.innerHTML = "Entered Metaverse"
+	e.parentNode.insertBefore(d, e);
+e.parentNode.removeChild(e);
 });
